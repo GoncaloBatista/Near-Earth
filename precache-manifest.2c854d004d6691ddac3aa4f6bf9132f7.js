3b9307379097c51c44797f04a9391b7f319268a5
GoncaloBatista <goncalofbatista@hotmail.com>
2020-06-02 15:07:23 +0100

    Updates

---
+++ precache-manifest.2c854d004d6691ddac3aa4f6bf9132f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d02d2016da0e163427070a1fdd2c9ee",
+    "url": "/near-earth/index.html"
+  },
+  {
+    "revision": "6500458cc7d82b927d78",
+    "url": "/near-earth/static/css/main.fbbe192a.chunk.css"
+  },
+  {
+    "revision": "136f9233c52ab56ca22d",
+    "url": "/near-earth/static/js/2.bb70c1bb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/near-earth/static/js/2.bb70c1bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6500458cc7d82b927d78",
+    "url": "/near-earth/static/js/main.df24a462.chunk.js"
+  },
+  {
+    "revision": "8b9beaf9bebfb9401d5c",
+    "url": "/near-earth/static/js/runtime-main.0c2e45ea.js"
+  }
+]);
